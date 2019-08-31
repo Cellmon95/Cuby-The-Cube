4870f8c361a7c1b2b125f6d7d908e4376e1c9b54
Lucas Källberg <lucas.arne@hotmail.com>
2019-08-31 18:47:36 +0200

    Fixed collisions and made so you could make maps with tiled

--- Cuby The Cube/js/Game.js
+++ Cuby The Cube/js/Game.js
@@ -6,18 +6,55 @@ var gameWindow = document.getElementById("gameWindow").getContext("2d");
 var gameObjects = [];
 var testGameObject;
 var player = new Player();             
-var floor = new Platform(new Vector2(0, 164), new Vector2(800, 100));
+var floor = new Platform(new Vector2(0, 564), new Vector2(800, 100));
 var platform = new Platform(new Vector2(150, 480), new Vector2(128, 64));
+var platform1 = new Platform(new Vector2(250, 380), new Vector2(128, 64));
+var platform2 = new Platform(new Vector2(350, 280), new Vector2(128, 64));
+var platform3 = new Platform(new Vector2(450, 180), new Vector2(128, 64));
+var mapLoaded = false;
+var tiles = [];
+var map = NaN;
 
 init();
 function init() {
 
     //testGameObject = new GameObject();
-    gameObjects = [platform, floor];
+    //gameObjects = [platform, platform1, platform2, platform3, floor];
+    loadMap();
 }
 
 function start() {
     
+}
+
+function loadMap() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            map = JSON.parse(this.responseText);
+            drawMap();
+            mapLoaded = true;
+
+        }
+    };
+    xhttp.open("GET", "test.json", true);
+    xhttp.send();
+
+}
+
+function drawMap() {
+    var idx = 0;
+
+    for (var i = 0; i < map.layers[0].height; i++) {
+        
+        for (var j = 0; j < map.layers[0].width; j++) {
+            if (map.layers[0].data[idx] === 1)
+            {
+                tiles.push(new Platform(new Vector2(j * 64, i * 64), new Vector2(64, 64)));
+            }
+            idx++;
+        }
+    }
 }
 
 // Physics handler
@@ -25,22 +62,34 @@ function start() {
 //events
 window.setInterval(tick, 16);
 
+
+
 function tick() {
-    draw();
-    update();
+    if (mapLoaded) {
+        gameObjects = tiles;
+        update();
+        draw();
+    }
+
 }
 
 function update() {
     player.update();
-    keyBoardHandlerReset();
     handleCollision();
+    keyBoardHandlerReset();
 }
 
 function draw() {
     gameWindow.clearRect(0, 0, 800, 600);
+    for (var i = 0; i < tiles.length; i++) {
+        tiles[i].draw(gameWindow);
+    }
     player.draw(gameWindow);
-    platform.draw(gameWindow);
-    floor.draw(gameWindow);
+    //platform.draw(gameWindow);
+    //platform1.draw(gameWindow);
+    //platform2.draw(gameWindow);
+    //platform3.draw(gameWindow);
+    //floor.draw(gameWindow);
 }
 
 function handleCollision() {
--- Cuby The Cube/js/game_objects/Player.js
+++ Cuby The Cube/js/game_objects/Player.js
@@ -27,7 +27,7 @@ class Player extends GameObject {
         this.jumpForce = 20;
 
         this.position.x = 50;
-        this.position.y = 500;
+        this.position.y = 400;
 
         this.movingRight = true;
         this.inertia = 2;
@@ -36,6 +36,8 @@ class Player extends GameObject {
         this.onGround = true;
 
         this.color = "#FF0000";
+
+        //Init sides
         //TODO:: need to have more const
         this.topSide = new GameObject();
         this.topSide.bounds = new Vector2(32, 1);
@@ -45,13 +47,15 @@ class Player extends GameObject {
         this.bottomSide = new GameObject();
         this.bottomSide.bounds = new Vector2(32, 1);
         this.bottomSide.position.x = this.position.x + 16;
-        this.bottomSide.position.y = this.position.y + this.bounds.y;//NOTE:: should be size and note bounds?
-        //this.rightSide = new PlayerSide();
-        //this.rightSide.position = new Vector2(this.bounds.x, this.bounds.y);
-        //this.rightSide.size = new Vector2(1, this.bounds.y + this.position.y);
-        //this.rightSide.onCollision = new function () {
-        //    console.log("i collided on the right!");
-        //};
+        this.bottomSide.position.y = this.position.y + this.bounds.y - 1;//NOTE:: should be size and note bounds?
+
+        this.rightSide = new GameObject();
+        this.rightSide.position = new Vector2(this.position.x + this.bounds.x, this.position.y);
+        this.rightSide.bounds = new Vector2(1, this.bounds.y);
+
+        this.leftSide = new GameObject();
+        this.leftSide.position = new Vector2(this.position.x, this.position.y);
+        this.leftSide.bounds = new Vector2(1, this.bounds.y - 5);
 
     }
     
@@ -83,15 +87,26 @@ class Player extends GameObject {
             this.bottomSide.bounds.x,
             this.bottomSide.bounds.y
         );
+
+        gameWindow.fillStyle = "#000000";
+        gameWindow.fillRect(
+            this.leftSide.position.x,
+            this.leftSide.position.y,
+            this.leftSide.bounds.x,
+            this.leftSide.bounds.y
+        );
+
+        gameWindow.fillStyle = "#000000";
+        gameWindow.fillRect(
+            this.rightSide.position.x,
+            this.rightSide.position.y,
+            this.rightSide.bounds.x,
+            this.rightSide.bounds.y
+        );
     }
 
     update() {
         this._handleMovementPhysics();
-        this.topSide.position.x = this.position.x + 16;
-        this.topSide.position.y = this.position.y;
-
-        this.bottomSide.position.x = this.position.x + 16;
-        this.bottomSide.position.y = this.position.y + this.bounds.y;
         //this.rightSide.position = new Vector2(this.bounds.x, this.bounds.y);
         //this.rightSide.size = new Vector2(this.bounds.y + this.position.y, 1);
         //this.color = "#FF0000";
@@ -134,9 +149,8 @@ class Player extends GameObject {
             }
             this.onGround = false;
         }
-        if (!this.onGround) {
-            this.velocityY += this.gravity;
-        }
+
+        this.velocityY += this.gravity;
 
 
         //if (this.position.y > 500) {
@@ -147,39 +161,54 @@ class Player extends GameObject {
         //    this.velocityY = 0;
         //    this.position.y = 500;
         //}
-        
-        this.position.x += this.velocityX;
-        this.position.y += this.velocityY;
+
+        this.move(this.velocityX, this.velocityY);
     }
 
     onCollision(target, arg) {
-        this.velocityX = 0;
-
         while (colliding(this.topSide, target)) {
             this.velocityY = 0;
-            this.position.y += 1;
-            this.topSide.position.y = this.position.y;
+            this.move(0, 1);
         }
 
         while (colliding(this.bottomSide, target)) {
             this.velocityY = 0;
             this.onGround = true;
-            this.position.y -= 1;
-            this.bottomSide.position.y = this.position.y;
+            this.move(0, -1);
         }
 
-        //while (colliding(this, target)) {
-        //    if (this.movingDirection === "RIGHT") {
-        //        this.position.x -= 1;
-        //    }
-        //    if (this.movingDirection === "LEFT") {
-        //        this.position.x += 1;
-        //    }
-        //}
+        while (colliding(this.rightSide, target)) {
+            this.velocityX = 0;
+            this.move(-1, 0);
+        }
+
+        while (colliding(this.leftSide, target)) {
+            this.velocityX = 0;
+            this.move(1, 0);
+        }
         
     }
 
+    setPosition(position) {
+        this.position = position;
 
+        this.topSide.position.x = this.position.x + 16;
+        this.topSide.position.y = this.position.y;
+
+        this.bottomSide.position.x = this.position.x + 16;
+        this.bottomSide.position.y = this.position.y + this.bounds.y - 1;
+
+        this.rightSide.position = new Vector2(this.position.x + this.bounds.x - 1, this.position.y);
+        this.leftSide.position = new Vector2(this.position.x, this.position.y);
+    }
+
+    move(x, y) {
+        this.setPosition(
+            new Vector2(
+                this.position.x + x, this.position.y + y
+            )
+        );
+    }
 
 
 }
